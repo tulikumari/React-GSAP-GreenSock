3919fa7446c070464ce84094c9d3be07d40677b1
tulikumari <info.tulikumari@gmail.com>
2020-09-24 11:20:29 +0530

    Updates

---
+++ precache-manifest.bf5b98dc4251361eaa96b71a6d361a55.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f0c9c246727cb5e90e49",
+    "url": "/React-GSAP-GreenSock/static/css/main.159541b4.chunk.css"
+  },
+  {
+    "revision": "f0c9c246727cb5e90e49",
+    "url": "/React-GSAP-GreenSock/static/js/main.c1c4413c.chunk.js"
+  },
+  {
+    "revision": "6691a0c9bfb60f550dde",
+    "url": "/React-GSAP-GreenSock/static/js/runtime~main.53ead9cc.js"
+  },
+  {
+    "revision": "15c694567200deb85a7f",
+    "url": "/React-GSAP-GreenSock/static/js/2.7ae6c869.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-GSAP-GreenSock/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a8813ecc5c473a735b0547a6c3a409d0",
+    "url": "/React-GSAP-GreenSock/index.html"
+  }
+];
